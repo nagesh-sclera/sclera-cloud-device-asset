3c6f8a5e39df94426124ac5c2d09328a23fac9fe
dhanush-vk-sclera <dhanush.vasantha@sclera.com>
2026-06-09 11:01:15 +0530

    feat(dapr-commons): add nullable vdmsId to SchedulerTriggerEvent

--- dapr-commons/src/test/java/io/sclera/dapr/events/SchedulerEventsTest.java
+++ dapr-commons/src/test/java/io/sclera/dapr/events/SchedulerEventsTest.java
@@ -2,14 +2,23 @@ package io.sclera.dapr.events;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class SchedulerEventsTest {
 
     @Test
-    void triggerEventCarriesJobNameAndRunId() {
-        SchedulerTriggerEvent e = new SchedulerTriggerEvent("snmpSync", "run-1", 123L);
-        assertEquals("snmpSync", e.jobName());
+    void triggerEventCarriesJobNameRunIdAndVdmsId() {
+        SchedulerTriggerEvent e = new SchedulerTriggerEvent("vdmsSystemHealth", "run-1", "vdms-7", 123L);
+        assertEquals("vdmsSystemHealth", e.jobName());
         assertEquals("run-1", e.runId());
+        assertEquals("vdms-7", e.vdmsId());
+        assertEquals(123L, e.firedAtEpochMs());
+    }
+
+    @Test
+    void legacyThreeArgConstructorLeavesVdmsIdNull() {
+        SchedulerTriggerEvent e = new SchedulerTriggerEvent("snmpSync", "run-1", 123L);
+        assertNull(e.vdmsId());
         assertEquals(123L, e.firedAtEpochMs());
     }
 
